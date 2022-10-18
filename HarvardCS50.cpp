67395d7c1afef8b23a66e2893b1c976ae0c70dd8
Filip <92389794+w60846@users.noreply.github.com>
2022-10-18 21:43:17 +0200

    Update HarvardCS50.cpp

--- HarvardCS50.cpp
+++ HarvardCS50.cpp
@@ -3,19 +3,19 @@
 using namespace std;
 
 
-struct osoba
+struct person
 {
-    struct osoba* rodzice[2];
-    char krew[2];
+    struct person* parents[2];
+    char blod[2];
 };
 
 
 const int GENERATIONS = 3;
 const int INDENT_LENGTH = 4;
 
-osoba* create_family(int generations);
-void print_family(osoba* p, int generation);
-void free_family(osoba* p);
+person* create_family(int generations);
+void print_family(person* p, int generation);
+void free_family(person* p);
 char random_allele();
 
 
@@ -25,7 +25,7 @@ int main(void)
     srand(time(NULL));
 
     
-    osoba* p = create_family(GENERATIONS);
+    person* p = create_family(GENERATIONS);
 
     
     print_family(p, 0);
@@ -35,124 +35,96 @@ int main(void)
 }
 
 
-osoba* create_family(int generations)
+person* create_family(int generations)
 {
     
   
-    osoba* w = new osoba;
+    person* w = new person;
 
 
     if (generations > 1)
-    {
-      
-       osoba* parent[2];
+    {      
+       person* parent[2];
+       
        parent[0] = create_family(generations - 1);
        parent[1] = create_family(generations - 1);
 
-  
+       w->parents[0] = parent[0];
+       w->parents[1] = parent[1];
 
-
-
-        w->rodzice[0] = parent[0];
-        w->rodzice[1] = parent[1];
-
-
- 
-
-        w->krew[0] = parent[rand() % 2]->krew[0];
-        w->krew[1] = parent[rand() % 2]->krew[1];
-
-        
-
+       w->krew[0] = parent[rand() % 2]->blod[0];
+       w->krew[1] = parent[rand() % 2]->blod[1];
     }
 
     // If there are no generations left to create
     else
     {
         // TODO: Set parent pointers to NULL
-        
-        
-        w->rodzice[0] = NULL;
-        w->rodzice[1] = NULL;
 
+        w->parents[0] = NULL;
+        w->parents[1] = NULL;
         
-        w->krew[0] = random_allele();
-        w->krew[1] = random_allele();
+        w->blod[0] = random_allele();
+        w->blod[1] = random_allele();
     }
 
-   
     return w;
 }
 
 
-void free_family(osoba* p)
+void free_family(person* p)
 {
     
-    if (p == NULL) {
+    if (p == NULL) 
         return;
-    }
 
     
-    free_family(p->rodzice[0]);
-    free_family(p->rodzice[1]);
-
-    
+    free_family(p->parents[0]);
+    free_family(p->parents[1]);
+  
     delete(p);
 
 }
 
 
-void print_family(osoba* p, int generation)
+void print_family(person* p, int generation)
 {
     
-    if (p == NULL)
-    {
+    if (p == NULL)    
         return;
-    }
-
     
-    for (int i = 0; i < generation * INDENT_LENGTH; i++)
-    {
+    for (int i = 0; i < generation * INDENT_LENGTH; i++)    
         cout<<(" ");
-    }
-
     
-    if (generation == 0)
-    {
-        cout << "Child (Generation " << generation << "): blood type " << p->krew[0] << p->krew[1]<<endl;
-    }
-    else if (generation == 1)
-    {
-        cout << "Parent (Generation " << generation << "): blood type " << p->krew[0] << p->krew[1] << endl;
-    }
+    if (generation == 0)   
+        cout << "Child (Generation " << generation << "): blood type " << p->blod[0] << p->blod[1]<<endl;
+    
+    else if (generation == 1)    
+        cout << "Parent (Generation " << generation << "): blood type " << p->blod[0] << p->blod[1] << endl;
+    
     else
     {
         for (int i = 0; i < generation - 2; i++)
         {
             cout<<("Great-");
         }
-        cout<<"Grandparent (Generation " << generation << "): blood type " << p->krew[0] << p->krew[1] << endl;
+        cout<<"Grandparent (Generation " << generation << "): blood type " << p->blod[0] << p->blod[1] << endl;
     }
 
-   
-    print_family(p->rodzice[0], generation + 1);
-    print_family(p->rodzice[1], generation + 1);
+    print_family(p->parents[0], generation + 1);
+    print_family(p->parents[1], generation + 1);
 }
 
 
 char random_allele()
 {
    short int r = rand() % 3;
-    if (r == 0)
-    {
+    if (r == 0)    
         return 'A';
-    }
-    else if (r == 1)
-    {
+    
+    else if (r == 1)    
         return 'B';
-    }
-    else
-    {
+    
+    else   
         return 'O';
-    }
 }
